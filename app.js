a6295b7932203eaa27c53601d4662b7768e4fefc
bharath_kumar <bharath292001@gmail.com>
2021-05-27 19:08:14 +0530

    Updating app.js and user.js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express')
-/*routes = require('./routes')*/
-, user = require('./routes/user.js')
+, user = require('./user.js')
 , http = require('http')
 , path = require('path');
-//const flash = require('express-flash')
-//const bodyParser = require('body-parser');
+const keys = require('./keys.js');
 const session = require('express-session');
 const uuid=require('uuid').v4
 const FileStore = require('session-file-store')(session);
@@ -13,15 +11,19 @@ const app = express();
 const mysql  = require('mysql');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy
-
-
-//app.use(flash())
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const nodeGeocoder = require('node-geocoder');
+let options = {
+    provider: 'openstreetmap'
+  };
+   
+let geoCoder = nodeGeocoder(options);
 
 var con=mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"Harish$30",
-    database:"blood_bank_system",
+    password:"bharatH?718",
+    database:"Bbank",
     insecureAuth:true
    
 });
@@ -37,7 +39,37 @@ global.db = con;
 global.bcrypt=bcrypt;
 global.mysql=mysql;
 
-// configure passport.js to use the local strategy
+// Google authentication for user
+passport.use(
+    new GoogleStrategy(
+      {
+        clientID:keys.clientId,
+        clientSecret:keys.clientSecret,
+        callbackURL: "/auth/google/redirect"
+       // accessType: "offline",
+       // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+      },
+      (accessToken, refreshToken, profile, done) => {
+        console.log('passport callback function');
+        console.log(profile.emails[0].value);
+        db.query("SELECT * FROM USER where email="+mysql.escape(profile.emails[0].value)+";",(err,result,fields)=>{
+            if(err){
+                console.log(err);
+                return done(null,false);
+            }
+            if(result.length==0){
+               console.log("User doesn't exist");
+                return done(null,false);
+            }
+            else{
+               return  done(null,result[0]);
+            }
+        });
+    }
+    )
+);
+
+//local strategy for user
 passport.use('user',new LocalStrategy(
     { usernameField: 'id' },
     (id, password, done) => {
@@ -46,37 +78,36 @@ passport.use('user',new LocalStrategy(
         console.log(result);
         if(err){
             console.log(err);
-            res.end(err['sqlMessage']);
+            return done(null, false, { message: "Password error" });
         }
-        if(result==={}){
-            //res.send("User doesn't exist");
+        if(result.length==0){
+            console.log("User doesn't exist");
+            return done(null, false, { message: "User doesn't exist" });
         }
         var found=0;
         var user;
         bcrypt.compare(password,result[0].password, function(err, ress){
             if(ress===true){
-                console.log("Login sucess");
+                console.log("Login success");
                 found=1;
                 user=result[0];
             }
-        console.log(found);
-        console.log("hello");
-        if(found==1){
-            console.log('Local strategy returned true')
-            return done(null, user)
-        }
-        else{
-           console.log('Wrong password')
-           message = 'You have entered invalid  password.';
-           //res.render('index.ejs',{message: message});
-           //reload the form page
-       }   
+            if(found==1){
+                console.log('Local strategy returned true')
+                return done(null, user)
+            }
+            else{
+                console.log('Wrong password')
+                return done(null,false,{message:"Wrong password"});
+          
+            }   
        })
        })
-   
     }
 ));
-/*passport.use('bbank',new LocalStrategy(
+
+//local strategy for Bbank
+passport.use('bbank',new LocalStrategy(
     { usernameField: 'id' },
     (id, password, done) => {
       console.log('Inside local strategy callback')
@@ -86,8 +117,9 @@ passport.use('user',new LocalStrategy(
             console.log(err);
             res.end(err['sqlMessage']);
         }
-        if(result==={}){
-            res.send("Bank doesn't exist");
+        if(result.length==0){
+            console.log("Bank doesn't exist");
+            return done(null, false, { message: "Bank doesn't exist" });
         }
         var found=0;
         var bank;
@@ -102,53 +134,78 @@ passport.use('user',new LocalStrategy(
                 return done(null, bank)
             }
             else{
-            console.log('Wrong password')
-            message = 'You have entered invalid  password.';
-            res.render('index.ejs',{message: message});
-            //reload the form page
+                console.log('Wrong password')
+                message = 'You have entered invalid  password.';
+                return done(null, false, { message: "Wrong password" });
+                //res.render('index.ejs',{message: message});
+                //reload the form page
              }   
        })
        })
     }
-));*/
+));
 
-// tell passport how to serialize the user
-passport.serializeUser((user, done) => {
-    if(Object.keys(user).length==8){
+// tell passport how to serialize the user and bbank
+passport.serializeUser((obj, done) => {
+    if(Object.keys(obj).length==9){
         console.log('Inside serializeUser callback. User id is save to the session file store here');
-        console.log(user.UserID);
-        done(null, user.UserID);
+        var a_id='a'+obj.UserID;//to diff user
+        console.log(a_id);
+        done(null,a_id);
+    }
+    else{
+        console.log('Inside serializeUser callback. Bbank id is save to the session file store here');
+        var b_id='b'+obj.UserID;//to diff bank
+        console.log(b_id);
+        done(null,b_id);
     }
 });
+//tell passport how to deserialize the user and bbank
 passport.deserializeUser((id, done) => {
     console.log('Inside deserializeUser callback')
-    console.log(`The user id passport saved in the session file store is: ${id}`)
-    db.query("SELECT * FROM User where UserID="+mysql.escape(id)+";",(err,result,fiels)=>{
-        console.log(result);
-        if(err){
-            console.log(err);
-            res.end(err['sqlMessage']);
-        }
-        const user=result[0];
-        done(null,user);
-    })
+    console.log("The user id passport saved in the session file store is: ${id}")
+    var l_id=id.toString();
+    console.log(l_id.charAt(0));
+    if(l_id.charAt(0)=='a'){
+        console.log("user"+l_id.substr(1));
+        db.query("SELECT * FROM User where UserID="+mysql.escape(l_id.substr(1))+";",(err,result,fiels)=>{
+            console.log(result);
+            if(err){
+                console.log(err);
+                res.end(err['sqlMessage']);
+            }
+            const obj=result[0];
+            done(null,obj);
+        })
+    }
+    else{
+        console.log("bank"+l_id.substr(1));
+        db.query("SELECT * FROM blood_bank where BloodbankID="+mysql.escape(l_id.substr(1))+";",(err,result,fiels)=>{
+            console.log(result);
+            if(err){
+                console.log(err);
+                res.end(err['sqlMessage']);
+            }
+            const obj=result[0];
+            done(null,obj);
+        })
+    }
 });
 
 //app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
-//app.use(bodyParser.urlencoded({ extended: false }))
-//app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+//session
 app.use(session({
     genid: (req) => {
         console.log('Inside the session middleware')
         console.log(req.sessionID)
         return uuid() // use UUIDs for session IDs
     },
-    store: new FileStore({logFn: function(){}}),
+    store: new FileStore(),
     secret: 'dbmsproj',
     resave: false,
     saveUninitialized: true
@@ -160,11 +217,10 @@ app.use(passport.session());
 
 var message='';
 
+//basic endpoints
 app.get('/', function(req, res) {
-    console.log("Home page");
-    
-  console.log("Session id is",req.sessionID);
-    //res.send(req.sessionID);
+    console.log("Home page"); 
+   console.log("Session id is",req.sessionID);
     res.render('home');
 });
 app.get('/user', (req, res) => {
@@ -174,21 +230,37 @@ app.get('/user', (req, res) => {
 app.get('/msg',(req,res)=>{
    res.render('msg');
 });
- 
-/*app.get('/', routes.index);*/
-app.get('/signup', user.signup);
-app.post('/signup', user.signup);
-app.get('/login', user.login_get);
-app.post('/login', user.login);
+
+//google auth endpoints
+app.get("/auth/google", passport.authenticate("google", {
+    scope: ["profile", "email"]
+}));
+
+app.get("/auth/google/redirect",passport.authenticate("google",{ failureRedirect: '/login' }),(req,res)=>{
+    console.log("here");
+    if(!req.user){
+        console.log("error");
+        res.redirect("/signup");
+    }
+    else{
+     //   res.send(req.user);
+        return res.redirect("/profile");
+    }
+});
+
+//user
+app.get('/usignup', user.signup);
+app.post('/usignup', user.signup);
+app.get('/ulogin', user.login_get);
+app.post('/ulogin', user.login);
+app.get('/profile', user.dashboard);
+app.get('/health_history',user.history);
+app.get('/logout', user.logout);
 
 app.get('/udonate',user.donate);
 app.post('/udonate',user.donate);
 app.get('/urequest',user.request);
 app.post('/urequest',user.request);
-app.get('/profile', user.dashboard);
-app.get('/logout', user.logout);
-
-
 app.get('/uavailable/:id',user.available);
 app.get('/campavail/:id',user.campavail);
 
@@ -198,15 +270,9 @@ app.get('/bsignup', user.bsignup);
 app.post('/bsignup', user.bsignup);
 app.get('/blogin', user.blogin);
 app.post('/blogin', user.blogin);
-app.listen(8080,()=>{
-    console.log('app is listening on port 8080');
+
+app.listen(3000,()=>{
+    console.log('app is listening on port 3000');
 });
 
 
-
-
-
-//res=this is working
-//login -->POST  sucess/fail
-//request -->POST  request obj created
-//donate -->POST  
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-//const LocalStrategy = require('passport-local').Strategy
+
 exports.signup=function(req,res){
     var message='';
     if(req.method=="POST"){
@@ -12,20 +12,18 @@ exports.signup=function(req,res){
             const id=Number(result[0].uid)+1;
             console.log(id,"created");
             let hash_v;
-            let pts=0;
             bcrypt.genSalt(10, function(err, salt) {
                 bcrypt.hash(password, salt, null, function(err, hash) {
                     hash_v=hash;
                     console.log(hash_v);
-                    db.query("INSERT INTO user Values ("+mysql.escape(id)+","+mysql.escape(hash_v)+","+mysql.escape(name)+","+mysql.escape(age)+","+mysql.escape(state)+","+mysql.escape(city)+","+mysql.escape(address)+ "," +mysql.escape(pts)+");",(err,result,fields)=>{
+                    db.query("INSERT INTO user Values ("+mysql.escape(id)+","+mysql.escape(hash_v)+","+mysql.escape(name)+","+mysql.escape(age)+","+mysql.escape(state)+","+mysql.escape(city)+","+mysql.escape(address)+");",(err,result,fields)=>{
                         if(err){
                             console.log(err);
                             res.end(err['sqlMessage']);
                         }
                         message = "Your account has been created succesfully.";
-			           res.render('msg.ejs',{message:message,id:id});
+			           res.render('msg.ejs',{message: message});
                     });
-                    
                     db.query("INSERT INTO health_history Values("+mysql.escape(id)+","+mysql.escape(type)+","+mysql.escape(sugar)+","+mysql.escape(bp)+","+mysql.escape(ht)+","+mysql.escape(wt)+","+mysql.escape(ailments)+",NULL);",(err,result,fields)=>{
                         if(err){
                             console.log(err);
@@ -38,78 +36,98 @@ exports.signup=function(req,res){
         });
     }
     else{
-        res.render('signup',{message:message});
+        res.render('usignup',{message:message});
     }       
 };
 
 
-exports.login_get=function(req,res){
-    if (req.method == "GET"){
+exports.login=function(req,res,next){
+    if(req.method=="POST"){
+    var message='';     
+    passport.authenticate('user', (err, user, info) => {
+        req.login(user, (err) => {
+          if(req.user){
+            console.log(`req.user: ${JSON.stringify(req.user)}`)
+            res.redirect('/profile');
+          }
+          else{
+                message = 'You have entered invalid  password.';
+                res.render('index.ejs',{message: message});
+                res.redirect('/ulogin',{message:message});
+          }
+        })
+      })(req, res, next);
+    }
+    else{
         console.log('Inside GET /login callback')
         console.log(req.sessionID)
-        //res.send(`You got the login page!\n`)
+        res.send(`You got the login page!\n`)
         res.render('index.ejs',{message:''});
     }
 };
 
-exports.login=function(req,res,next){
-    var message='';     
-    console.log(req.body.id);
-    console.log(req.body.password);
-//res.send('You were authenticated & logged in!\n');
-    passport.authenticate('user', (err, user, info) => {
-        console.log('Inside passport.authenticate() callback');
-        console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
-        console.log(`req.user: ${JSON.stringify(req.user)}`)
-        req.login(user, (err) => {
-          console.log('Inside req.login() callback')
-          console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
-          console.log(`req.user: ${JSON.stringify(req.user)}`)
-            res.redirect('/profile');
-            return;
-        })
-      })(req, res, next);
-    // res.redirect('/profile');
-    
-    };
-
-
-
 exports.dashboard=function(req,res){
-        if(req.isAuthenticated()){
+    if(req.isAuthenticated()) {
         const id=req.user.UserID;
-        console.log("YEAHHHHH",id);
-        res.send('you hit the authentication endpoint\n');
-     
-        res.render('user_dashboard.ejs');
-        }
+        console.log(id);
+        res.render('user_dashboard.ejs',{id:id});
+      } 
       else {
-          console.log("Eroor\n");
-        res.redirect('/login');
-        return;
+        res.redirect('/ulogin',{message:''});
+      }
+}; 
+
+
+exports.history=function(req,res){
+    if(req.isAuthenticated()) {
+        const id=req.user.UserID;
+        console.log(id);
+        var obj;
+        db.query("SELECT * from health_history where UserID="+mysql.escape(id)+";",(err,result,field)=>{
+            if(err){
+                console.log(err);
+                res.end(err['sqlMessage']);
+            }
+            if(result.length==0){
+                res.send("User doesn't exist");
+            }
+            obj=result[0];
+            console.log(obj);
+        })
+        db.query("SELECT pts as pt from User where UserID="+mysql.escape(id)+";",(err,result,field)=>{
+            if(err){
+                console.log(err);
+                res.end(err['sqlMessage']);
+            }
+            if(result.length==0){
+                res.send("User doesn't exist");
+            }
+            obj["points"]=result[0].pt
+            console.log(obj);
+            res.send(obj);
+        })
+      } 
+      else {
+         res.redirect('/ulogin',{message:''});
       }
 
-
-}; 
+}
 
 exports.donate=function(req,res){
     var id;
     var found=0;
     if(req.method=="POST"){
         if(req.isAuthenticated()) {
+            res.send('you hit the authentication endpoint\n');
             const id=req.user.UserID;
             found=1;
-            console.log('Donated id is',id);
-           // res.send('you hit the authentication endpoint\n');
-            
         }
         else {
-        res.redirect('/login',{message:''});
-        return;
+            res.redirect('/ulogin',{message:''});
         }
         if(found==1){
                 var messgae='';
-                const {date,bname}=req.body;
+                var {date,bname}=req.body;
                 console.log(bname);
                 console.log(typeof(bname));
                 db.query("SELECT MAX(DonationID) as did FROM donations;",(err,result,fields)=>{
@@ -120,20 +138,17 @@ exports.donate=function(req,res){
                 let did=result[0].did+1;
                 //console.log(did,"created");
                 db.query("SELECT BloodBankID FROM blood_bank WHERE Name="+mysql.escape(bname)+";",(err,result,fields)=>{
-                    let bbid=result[0].BloodBankID;
+                    let bbid=result[0].BloodbankID;
                     let status="Pending";
-                    let uid=req.user.UserID;
-                    console.log(bbid);
+                    let uid=req.params.id;
+                    //console.log(uid);
                     db.query("INSERT INTO donations Values("+mysql.escape(did)+","+mysql.escape(uid)+","+mysql.escape(bbid)+","+mysql.escape(date)+","+mysql.escape(status)+");",(err,result,fields)=>{
                         if(err){
                             console.log(err);
                             res.end(err['sqlMessage']);
                         }
-                    
-                    message="Donation request submitted";
-                    
-                    //res.render('blood_donor.ejs',{message:message,id:''});
-                    res.redirect('/profile');
+                        message="Donation request submitted";
+                        res.redirect("/profile",{message:message});
                         //res.end("Success");
                         //alert blood bank,increment donation counts
                         //trigger to increment points
@@ -148,8 +163,7 @@ exports.donate=function(req,res){
         console.log("id=",id);
         var message='';
         res.render('blood_donor.ejs',{id:id,message:message});
-    }
-    
+    }    
 };
 
 exports.request=function(req,res){
@@ -157,13 +171,11 @@ exports.request=function(req,res){
     var found=0;
     if(req.method=="POST"){
         if(req.isAuthenticated()) {
-            res.send('you hit the authentication endpoint\n');
+            console.log('you hit the authentication endpoint\n');
             const id=req.user.UserID;
             found=1;
-        //res.render('user_dashboard.ejs',{id:id});
         } else {
-            res.redirect('/login'),{message:''};
-            return;
+            res.redirect('/ulogin',{message:''});
         }
         if(found==1){
             const {type,comp,units,area,name}=req.body;
@@ -203,211 +215,4 @@ exports.request=function(req,res){
         var message='';
         res.render('blood_request.ejs',{id:id,message:message});
     }  
-};
-
-
-
- exports.available=function(req,res){
-    if(req.method=="GET"){
-       
-    let type=req.query.bloodtype;
-    let comp=req.query.bloodcomp;
-    let state=req.query.state;
-    let city=req.query.city;
-    console.log(type);
-    console.log(comp);
-    console.log(state);
-
-   
-        db.query("SELECT U_AVAIL("+mysql.escape(city)+","+mysql.escape(state)+","+mysql.escape(type)+","+mysql.escape(comp)+") as avail;",(err,result,fields)=>{
-            if(err){
-                console.log(err);
-                res.end(err['sqlMessage']);
-            }
-            console.log("Blood details shown");
-            //res.send(JSON.parse(result[0].avail));
-            res.render('disp.ejs',{data:JSON.parse(result[0].avail)});
-        });
-    }
-    else{
-        res.render('blood_stock.ejs');
-    }
-
-     
-    
-};
-exports.campavail=function(req,res){
-    if(req.method=="GET"){
-    let uid=req.params.id;
-    
-        db.query("SELECT city,state FROM USER WHERE Userid="+mysql.escape(uid)+";",(err,result,fields)=>{
-            if(err){
-                console.log(err);
-                res.end(err['sqlMessage']);
-            }
-            console.log(result[0].state);
-            city=result[0].city;
-            state=result[0].state;
-            db.query("SELECT *  FROM CAMPS WHERE City="+mysql.escape(city)+"AND State="+mysql.escape(state)+";",(err,resu,fields)=>{
-                if(err){
-                    console.log(err);
-                    res.end(err['sqlMessage']);
-                }
-                //display camps
-                console.log(resu);
-                res.send(resu);
-            });
-        });
-        //display all camps with an option
-    }
-    else{
-        res.render('blood_camp.ejs');
-    }
-
-
-};
-
-exports.logout=function(req,res){
-    req.session.destroy(function(err) {
-       return res.redirect('/login');
-    })
- };
-
-
-//BLOODBANK
-
-
-exports.bsignup=function(req,res){
-    var message='';
-    if(req.method=="POST"){
-    const {name,password,license,state,city,cat,contact,address,website}=req.body;
-    
-    var hash_v;
-    
-        //let pts=0;
-        let id;
-       // var hash_v;
-        db.query("SELECT MAX(BloodBankID) as bbid FROM blood_bank;",(err,result,fields)=>{
-            if(err){
-                console.log(err);
-                res.end(err['sqlMessage']);
-            }
-            id=result[0].bbid+1;
-            console.log(id,"created");
-            bcrypt.genSalt(10, function(err, salt) {
-                bcrypt.hash(password, null, null, function(err, hash) {
-                        hash_v=hash;
-            
-                    db.query("INSERT INTO blood_bank Values ("+mysql.escape(id)+","+mysql.escape(hash_v)+","+mysql.escape(name)+","+mysql.escape(cat)+","+mysql.escape(address)+","+mysql.escape(contact)+","+mysql.escape(license)+","+mysql.escape(website)+","+mysql.escape(state)+","+mysql.escape(city)+");",(err,result,fields)=>{
-                        if(err){
-                            console.log(err);
-                            res.end(err['sqlMessage']);
-                        }
-                        message = "Your account has been created succesfully.";
-			           res.render('bmsg.ejs',{message: message});
-                        //res.end(1);
-                    });
-                });
-            });
-        });
-    }
-        else{
-            res.render('bsignup.ejs',{message:message});
-        }
-    
-};
-exports.blogin=function(req,res){
-    var message='';
-    var sess=req.session;
-    if(req.method=="POST"){
-    const {name,password}=req.body;
-    var sql="SELECT BloodBankID, password FROM `blood_bank` WHERE `Name`='"+name+"'"; 
-        db.query(sql,(err,result,fields)=>{
-            if(err){
-                console.log(err);
-                res.end(err['sqlMessage']);
-            }
-            if(result.length){
-            var found=0;
-            var hash_v;
-            console.log(result[0].password);
-            bcrypt.compare(password,result[0].password, function(err, resu){
-               // console.log(hash_v);
-                if(resu===true){
-                   // console.log(resu[0].password);
-                    console.log("Login sucess");
-                    found=1;
-                }
-                //res.end("Login Success");
-                if(found==1){
-                   // res.end("Login Sucesss");
-                   req.session.bId = result[0].BloodBankID;
-                    var id=req.session.bId;
-                    req.session.buser = result[0];
-                   res.redirect(`/bprofile/${id}`);
-                   return;
-                    //route to url /:id/bprofile
-                }
-                else{
-                    message = 'You have entered invalid  password.';
-                    res.render('blogin.ejs',{message: message});
-                }
-            });
-        }
-        else{
-            message = 'You have entered invalid username or password.';
-           res.render('blogin.ejs',{message: message});
-           }
-         });
-    
-    
-  }
-  else{
-    res.render('blogin.ejs',{message: message});
-  }
-};
-
-exports.bdashboard=function(req,res){
-    if(req.session.bId){
-    console.log(req.session.bId);
-    const {id}=req.params;
-
-    res.render('bbank_dashboard.ejs',{id:id});
-    }
-    else{
-       res.render('msg2.ejs');
-    }
-}; 
-exports.campreq=function(req,res){
-    
-    if(req.method=="POST"){
-         const {city,state,date}=req.body;
-  
-        db.query("SELECT MAX(CampID) as cid from CAMPS",(err,result,field)=>{
-            if(err){
-                console.log(err);
-                res.end(err['sqlMessage']);
-            }
-            var cid,bid;
-            if(!result.length){
-                cid=1;
-            }
-            cid=result[0].cid+1;
-            bid=req.params.id;
-            status="Pending";
-            db.query("INSERT INTO CAMPS VALUES("+mysql.escape(cid)+","+mysql.escape(bid)+","+mysql.escape(date)+","+mysql.escape(status)+","+mysql.escape(city)+","+mysql.escape(state)+");",(err,result,fields)=>{
-                if(err){
-                    console.log(err);
-                    res.end(err['sqlMessage']);
-                }
-                message = "Camp registered";
-                res.render('msg.ejs',{message: message});
-
-
-            });
-        });
-    }
-  else{
-        res.render("campreq");
-   }
 };
